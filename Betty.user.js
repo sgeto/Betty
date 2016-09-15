57096eec551a5d684326158efbba6cddcea32264
Ali Abdulkadir <autostart.ini@gmail.com>
2016-09-15 09:27:12 +0300

    Third commit

--- Betty.user.js
+++ Betty.user.js
@@ -2,16 +2,16 @@
 // @name            Betty
 // @namespace       autostart.ini@gmail.com
 // @author          Ali Abdulkadir (sgeto)
-// @version         0.3
+// @version         0.4
 // @license         GPL version 3 or any later version; http://www.gnu.org/licenses/gpl-3.0.txt
-// @description     A userscript that assists in finding open directories with Google.
+// @description     A user script that assists in finding open directories with Google.
 // @copyright       Jorge Frisancho (teocci), Ali Abdulkadir (sgeto)
-// @icon            https://paxadiutor.blogspot.com/favicon.ico
+// @icon            https://1.bp.blogspot.com/-VCFxO5wbOYw/V9optetVKcI/AAAAAAAABmE/g4ruvAETn8gxNbXoHZA21rYUogj45nzcwCLcB/s320/betty-space%2Binvader%2Bemoji.png
 // @homepage        Diarium
 // @homepageURL     https://goo.gl/DrRSGH
-// @updateURL       http://ggggg.js
-// @downloadURL     http://ggggr.js
-// @contributionURL https://github.com/teocci/GooglePirateExtended/wiki/Donate
+// @updateURL       https://raw.githubusercontent.com/sgeto/Betty/master/Betty.user.js
+// @downloadURL     https://raw.githubusercontent.com/sgeto/Betty/master/Betty.user.js
+// @contributionURL https://github.com/sgeto/Betty
 // @match           https://*.google.com/*
 // @include         https://*.google.com/*
 // @exclude         https://*.images.google.com/*
@@ -20,30 +20,31 @@
 // @grant           GM_xmlhttpRequest
 // ==/UserScript==
 function newradio(nametext, dorkvalue) {
-    var search = document.getElementsByName('f') [0];
-    var sometext = document.createTextNode(nametext);
-    var someradio = document.createElement('input');
-    var breakup = document.createElement('br');
-    someradio.setAttribute('type', 'radio');
-    someradio.setAttribute('name', 'q');
-    someradio.setAttribute('value', dorkvalue);
-	if (nametext==='Web'){
-	someradio.setAttribute('checked', 'checked');
-	}
-    search.appendChild(breakup);
-    search.appendChild(someradio);
-    search.appendChild(sometext);
-}
+  var search = document.getElementsByName('f') [0];
+  var sometext = document.createTextNode(nametext);
+  var someradio = document.createElement('input');
+  var breakup = document.createElement('br');
+  someradio.setAttribute('type', 'radio');
+  someradio.setAttribute('name', 'q');
+  someradio.setAttribute('value', dorkvalue);
+  // This statement is to automatically set the radio button to to "Web" at launch. It unfortunately breaks the Google Search button.
+  if (nametext === 'Web') {
+    someradio.setAttribute('checked', 'checked');
+  }
+  search.appendChild(breakup);
+  search.appendChild(someradio);
+  search.appendChild(sometext);
+}// I have no idea what this is doing.
 
-function newselect(nametext, dork){
-	var newoption = document.createElement('option');
-	newoption.setAttribute('value', dork);
-	newoption.innerHTML=nametext;
-	s.appendChild(newoption);
-}
+function newselect(nametext, dork) {
+  var newoption = document.createElement('option');
+  newoption.setAttribute('value', dork);
+  newoption.innerHTML = nametext;
+  s.appendChild(newoption);
+}//If I add more options, I'll eventually need to find a way to add a column break. There's also a max character limit for google searches that need to be taken care of for long queries
 
-if((document.title==='Google')){
-  document.getElementsByName('q')[0].focus();
+if ((document.title === 'Google')) {
+  document.getElementsByName('q') [0].focus();
   newradio('Web', '');
   newradio('Music', 'intitle:"music" (mp3|flac|wav|m4a|ogg|wma) "Parent Directory" -htm -html -asp -php -listen77 -idmusic -airmp3 -shexy -vmp3');
   newradio('Movies/TV', '(avi|mpg|wmv|mpeg|mp4|mkv|m4v|mov|flv|ogv|wmv|webm) "Parent Directory" -"Trailer" -cdkey -asp -torrent -html -web-shelf -zoozle -jsp -htm -listen77 -idmovies -shexy -eucontest -0x7');
@@ -53,16 +54,12 @@ if((document.title==='Google')){
   newradio('Archives', '(rar|zip|tar|iso|cso|gz|7z|bz2|gz|gzip|img) -torrent +intitle:"index.of"');
   newradio('(Mobile) Apps', '(exe|msi|msu|apk|deb) -torrent +intitle:"index.of"');
 } else {
-  function newselect(nametext, dork) {
-    var newoption = document.createElement('option');
-    newoption.setAttribute('value', dork);
-    newoption.innerHTML = nametext;
-    s.appendChild(newoption);
-  }
+  
   var s = document.createElement('select');
   s.setAttribute('name', 'q');
   s.setAttribute('onchange', 'window.location.href=window.location.href.split("&q=")[0]+"&q="+window.location.href.split("&q=")[1].split("&")[0]+"&q="+this.value');
   document.getElementById('prs').appendChild(s);
+  // The contents in both of these (newradio and newselect) have to be identical?!
   newselect('Web', '');
   newselect('Music', 'intitle:"music" (mp3|flac|wav|m4a|ogg|wma) "Parent Directory" -htm -html -asp -php -listen77 -idmusic -airmp3 -shexy -vmp3');
   newselect('Movies/TV', '(avi|mpg|wmv|mpeg|mp4|mkv|m4v|mov|flv|ogv|wmv|webm) "Parent Directory" -"Trailer" -cdkey -asp -torrent -html -web-shelf -zoozle -jsp -htm -listen77 -idmovies -shexy -eucontest -0x7');
@@ -71,6 +68,7 @@ if((document.title==='Google')){
   newselect('EBooks/Comics', '(chm|pdf|cbr|nfo|epub) -torrents -torrent -md5 -md5sums -idpdf');
   newselect('Archives', '(rar|zip|tar|iso|cso|gz|7z|bz2|gz|gzip|img) -torrent +intitle:"index.of"');
   newselect('(Mobile) Apps', '(exe|msi|msu|apk|deb) -torrent +intitle:"index.of"');
+  // the next few statements seem to discard certain results based on keywords in their URL's. Which we already told Google to do for us...
   if (window.location.href.search('idmusic') > 0) {
     s.options[1].defaultSelected = 'true';
   }
@@ -110,4 +108,5 @@ if((document.title==='Google')){
   ni.setAttribute('name', 'q');
   ni.setAttribute('value', s.value);
   document.forms[0].appendChild(ni);
-}inurl:youtube;
+}
+inurl: youtube;
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # Betty
-A userscript that assists finding open directories with Google.
+A User Script, which uses Google, to find open directories.
 
-This userscript started as gift for a friend. It is inspired and partial based on teocci's GooglePirateExtended. 
+This User Script started as gift for a friend. It is inspired and partial based on teocci's GooglePirateExtended.
+
+A User Scripts allows to customize the way a web page displays or behaves, by using small bits of JavaScript.
+
+From Greasyfork https://greasyfork.org):
+
+What are User Scripts?
+
+User Scripts put you in control of your browsing experience. Once installed, they automatically make the sites you visit better by adding features, making them easier to use, or taking out the annoying bits.
+
+How do I install User Scripts?
+
+To use user scripts you need to first install a user script manager. For Firefox, the best choice is Greasemonkey https://addons.mozilla.org/firefox/addon/greasemonkey/. For Chrome: Tampermonkey https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo. Other options exist https://greasyfork.org/en/help/installing-user-scripts.
+
+
+Additial information will be available on my blog https://goo.gl/DrRSGH soon.
+If you need help, please contact me there or open an issue.
